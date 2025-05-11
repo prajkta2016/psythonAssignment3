ab813750eeb05f0fb05d2e0c738eba81c774cbd6
prajkta2016 <prajktachitranshi09@gmail.com>
2025-05-11 14:06:46 +0530

    assignment 3 task 2

---
+++ assignment 3 task 2.py
@@ -0,0 +1,8 @@
+import math
+n=int(input("enter the number"))
+a=math.sqrt(n)
+print(a,"is the square root of ",n)
+b=math.log(n)
+print(b,"is the logarithm of ",n)
+c=math.sin(n)
+print(c,"is the cosine of ",n)
